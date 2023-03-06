fba4ecd1543d38f574f5fa8fca180dc4339c1675
marrcelosantana <marcelosantana@alu.ufc.br>
2023-03-06 18:30:16 -0300

    [Market-Space] - Add One Signal

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import OneSignal from "react-native-onesignal";
 
-import { Routes } from './src/routes';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { Routes } from "./src/routes";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
+
+import { CartContextProvider } from "./src/contexts/CartContext";
+
+OneSignal.setAppId("891154b1-2ca8-472d-87e5-838a0bb7e13c");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
